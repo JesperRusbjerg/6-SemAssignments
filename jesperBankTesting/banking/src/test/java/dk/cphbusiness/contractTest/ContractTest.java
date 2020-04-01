bdca27629abeb2009f0b4c859f9df119ca2c9012
jesper <jrusbjerg@hotmail.dk>
2020-04-01 14:45:44 +0200

    yep

--- jesperBankTesting/banking/src/test/java/dk/cphbusiness/contractTest/ContractTest.java
+++ jesperBankTesting/banking/src/test/java/dk/cphbusiness/contractTest/ContractTest.java
@@ -4,7 +4,9 @@ import dk.cphbusiness.facade.AccountFacadeDummy;
 import dk.cphbusiness.facade.BankFacadeDummy;
 import dk.cphbusiness.facade.CustomerFacadeDummy;
 import facadeTest.AccountFacadeTest;
+import facadeTest.BankFacadeTest;
 import facadeTest.ContractHolder;
+import facadeTest.CustomerFacadeTest;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -12,15 +14,17 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        AccountFacadeTest.class
-//        BankFacadeDummy.class,
-//        CustomerFacadeDummy.class
+        AccountFacadeTest.class,
+        BankFacadeTest.class,
+        CustomerFacadeTest.class
 })
-public class ContractTest {
 
+public class ContractTest {
     @BeforeClass
     public static void before(){
         ContractHolder.accountContract = new AccountFacadeDummy();
+        ContractHolder.bankContract = new BankFacadeDummy();
+        ContractHolder.customerContract = new CustomerFacadeDummy();
     }
 
 }
